63ce809ce0b59c9e367202269dffdb64a5c6545e
SpeeDark <nikitanam667@gmail.com>
2026-09-04 22:03:40 +0300

    Add refresh-token metadata field

--- src/Interceptors/MetadataInterceptor.cs
+++ src/Interceptors/MetadataInterceptor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Security.Claims;
 using Grpc.Core;
@@ -25,18 +26,24 @@ public class MetadataInterceptor : Interceptor
         var userId = httpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         var role = httpContext?.User?.FindFirst(ClaimTypes.Role)?.Value;
         var token = httpContext?.Request.Headers[HeaderNames.Authorization].FirstOrDefault()?.Split(" ").Last();
+        var refreshToken = httpContext?.Request.Headers["refreshToken"].FirstOrDefault();
+        
+        Console.WriteLine("Interceptor");
+        Console.WriteLine($"userId: {userId}");
+        Console.WriteLine($"role: {role}");
+        Console.WriteLine($"token: {token}");
+        Console.WriteLine($"refreshToken: {refreshToken}");
         
         var metadata = new Metadata();
         
         if (!string.IsNullOrEmpty(userId))
             metadata.Add("user-id", userId);
-        
         if (!string.IsNullOrEmpty(role))
             metadata.Add("user-role", role);
-        
         if (!string.IsNullOrEmpty(token))
             metadata.Add("authorization", $"Bearer {token}");
-        
+        if (!string.IsNullOrEmpty(refreshToken))
+            metadata.Add("refresh-token", refreshToken);
         if (httpContext?.TraceIdentifier != null)
             metadata.Add("request-id", httpContext.TraceIdentifier);
         
